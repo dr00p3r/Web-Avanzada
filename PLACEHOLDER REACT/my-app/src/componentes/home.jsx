787db6ede0f725346b53a853972d55a187b1b7bb
Joan Oswaldo Cobeña Zambrano <joancobe64@gmail.com>
2024-11-17 01:07:05 -0500

    Update home.jsx

--- PLACEHOLDER REACT/my-app/src/componentes/home.jsx
+++ PLACEHOLDER REACT/my-app/src/componentes/home.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes, useNavigate  } from 'react-rout
 import axios from 'axios';
 
 export default function Home(){
-
     return(
         <Router>
             <Routes>
@@ -21,28 +20,24 @@ async function HomeActions(){
     console.log(userToken);
 
     const navigate = useNavigate();
-    if(data.logIn){
-        return(
-            <h1 className="container text-center mt-5">HOLIII {data.username}!</h1>
-        );
-    }else{
-        return(
-            <div className="container text-center mt-5">
-                <h1>HOLIII</h1>
-                <div className="mt-4">
-                    <div className="mb-3">
-                        <button onClick={() => navigate('/register')}
-                            className="btn btn-primary btn-lg"> Registrarse </button>
-                    </div>
-                    <div className="mb-3">
-                        <button onClick={() => navigate('/login')} 
-                            className="btn btn-secondary btn-lg"> Iniciar Sesión </button>
-                    </div>
+
+    return(
+        <div className="container text-center mt-5">
+            <h1>HOLIII</h1>
+            <div className="mt-4">
+                <div className="mb-3">
+                    <button onClick={() => navigate('/register')}
+                        className="btn btn-primary btn-lg"> Registrarse </button>
                 </div>
-                <div id='frmContainer'></div>
+                <div className="mb-3">
+                    <button onClick={() => navigate('/login')} 
+                        className="btn btn-secondary btn-lg"> Iniciar Sesión </button>
+                </div>
             </div>
-        );
-    }
+            <div id='frmContainer'></div>
+        </div>
+    );
+
 
 }
 
