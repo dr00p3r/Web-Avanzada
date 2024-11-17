50551932b57f531297d20cde5076b8462373eb5f
Joan Oswaldo Cobeña Zambrano <joancobe64@gmail.com>
2024-11-17 13:03:12 -0500

    Update form.jsx

--- PLACEHOLDER REACT/my-app/src/componentes/form.jsx
+++ PLACEHOLDER REACT/my-app/src/componentes/form.jsx
@@ -4,7 +4,7 @@ function API_FORM({children, data}){
         <form id="frmSign" className="form container mt-5" onSubmit={data.onSubmit}>
             <div className='form-container card p-4 shadow-sm'>
                 {children}
-                <button className='button-submit btn-primary w-100 mt-3'>{data.msg}</button>
+                <button className='button-submit btn btn-primary w-100 mt-3'>{data.msg}</button>
             </div>
         </form>
     );
