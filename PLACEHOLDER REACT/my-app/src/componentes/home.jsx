4ebe5085cc6f904d2d625f835173d42a46461de8
Joan Oswaldo Cobeña Zambrano <joancobe64@gmail.com>
2024-11-17 00:56:41 -0500

    Update home.jsx

--- PLACEHOLDER REACT/my-app/src/componentes/home.jsx
+++ PLACEHOLDER REACT/my-app/src/componentes/home.jsx
@@ -3,8 +3,8 @@ import FrmRegister from './reg_form';
 import { BrowserRouter as Router, Route, Routes, useNavigate  } from 'react-router-dom';
 import axios from 'axios';
 
-export default async function Home(){
-    const userToken = await axios.get(process.env.COOKIE_ROUTE);
+export default function Home(){
+    const userToken = async ()=> await axios.get(process.env.COOKIE_ROUTE);
     console.log(userToken);
 
     return(
