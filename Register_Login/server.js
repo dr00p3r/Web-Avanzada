1148c7c02dec37adc6bab6c9f3d9bad9d80d1b2c
Joan Oswaldo Cobeña Zambrano <joancobe64@gmail.com>
2024-11-17 01:39:21 -0500

    Update server.js

--- Register_Login/server.js
+++ Register_Login/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'https://web-avanzada-eight.vercel.app/',
+    origin: 'https://web-avanzada-eight.vercel.app',
     credentials: true
 }));
 
